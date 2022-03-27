ad74befc53b231458e5c4d94c1860538e222f40e
Ryan Bieker <rbieke1@hotmail.com>
2022-03-27 16:59:50 -0500

    Add project files.

---
+++ MonkeyBusiness/MonkeyBusiness.cpp
@@ -0,0 +1,31 @@
+// Ryan Bieker, CS 3060 001, Chapter 7 Programming Challenges, due 3/29/2022, 
+//calculates average, least, and most amount of food eaten by monkeys at a zoo
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int monkeys[3][7];
+
+    for (int i = 0; i < monkeys)
+}
+
+//average food eaten per day by whole family of monkeys
+void average()
+{
+
+}
+
+//least food eaten during week by any one monkey
+void least()
+{
+
+}
+
+//greatest food eaten during week by any one monkey
+void most()
+{
+
+}
